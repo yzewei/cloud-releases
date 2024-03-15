16e841e3fe1c85d81826369ded4dea69a8c096d1
yzewei <yangzewei@loongson.cn>
2024-03-15 15:03:58 +0800

    add distribution 2.7.1
    Signed-off-by: yzewei <yangzewei@loongson.cn>

---
+++ distribution/distribution/2.7.1/ci.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+bash /lib/set-ftp.sh
+
+echo "set go env"
+	go env -w GOFLAGS="-buildvcs=false"
+#	export GOPROXY=https://goproxy.cn
+	export https_proxy="http://10.130.0.20:7890"
+
+echo "start build--------------------------------"
+		cd /build/${ORG}-${PROJECT} && \
+		make binaries && \
+		tar zcvf $PROJECT-linux-loong64.tar.gz bin && \
+		bash /build/lib/curl.sh *-linux-loong64.tar.gz $TAG $VERSION 
+

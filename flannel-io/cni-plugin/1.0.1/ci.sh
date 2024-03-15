d8399fcaf654adc67f21e080d578d3f8c2ed292d
yzewei <yangzewei@loongson.cn>
2024-03-15 11:03:09 +0800

    add flannel-io/cni-plugin 1.0.1 1.2.0
    Signed-off-by: yzewei <yangzewei@loongson.cn>

---
+++ flannel-io/cni-plugin/1.0.1/ci.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+bash /lib/set-ftp.sh
+
+echo "set go env"
+	go env -w GOFLAGS="-buildvcs=false"
+#	export GOPROXY=https://goproxy.cn
+	export https_proxy="http://10.130.0.20:7890"
+
+echo "start build--------------------------------"
+		cd /build/${ORG}-${PROJECT} && \
+			ARCH=loong64 make build_linux && mv /root/go/src/github.com/flannel-io/cni-plugin/dist/flannel-loong64 /build/${ORG}-${PROJECT}/ && \
+		tar zcvf $PROJECT-linux-loong64.tar.gz flannel-loong64 && \
+		bash /build/lib/curl.sh *-linux-loong64.tar.gz $TAG $VERSION 
+

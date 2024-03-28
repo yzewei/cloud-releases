e480b06465e2a7c6cc98339b80f865db0f2f65cc
yzewei <yangzewei@loongson.cn>
2024-03-28 14:05:20 +0800

    add bpftrace 0.20.3
    Signed-off-by: yzewei <yangzewei@loongson.cn>

---
+++ bpftrace/bpftrace/0.20.3/ci.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+bash /lib/set-ftp.sh
+
+	export https_proxy="http://10.130.0.20:7890"
+
+echo "start build--------------------------------"
+	cd /build/libbpf && cd src && make install_uapi_headers
+	cd /build/$PROJECT && \
+		#git apply ../0001-add-static.patch && \
+		mkdir build && cd build && \
+		cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF ../ && \
+		make -j8 && cd /build/$PROJECT/build/src && \
+		tar zcvf $PROJECT-linux-loong64.tar.gz  bpftrace && \
+		bash /build/lib/curl.sh *-linux-loong64.tar.gz $TAG $VERSION 
+

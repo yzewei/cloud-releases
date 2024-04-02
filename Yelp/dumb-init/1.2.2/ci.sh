8b3b97490fb1c9c501ba4aa034bfe8799925882b
yzewei <yangzewei@loongson.cn>
2024-04-02 15:52:19 +0800

    update dumb-init 1.2.2
    Signed-off-by: yzewei <yangzewei@loongson.cn>

--- Yelp/dumb-init/1.2.2/ci.sh
+++ Yelp/dumb-init/1.2.2/ci.sh
@@ -3,6 +3,8 @@
 set -o errexit
 set -o nounset
 
+bash lib/set-ftp.sh
+
 git clone --branch v1.2.2 --depth=1 https://github.com/Yelp/dumb-init.git
 
 mkdir dist
@@ -13,3 +15,5 @@ make build
 
 cp dumb-init ../dist/dumb-init-loongarch64
 
+tar zcvf dumb-init-linux-loong64.tar.gz dumb-init && \
+                bash /build/lib/curl.sh dumb-init-linux-loong64.tar.gz Yelp/dumb-init 1.2.2 

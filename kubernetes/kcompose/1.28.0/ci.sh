cd8b0481bbb8ebd15d5aaa5d727cc0e1f1bd53ec
yzewei <yangzewei@loongson.cn>
2024-11-25 17:56:31 +0800

    mend

--- kubernetes/kcompose/1.28.0/ci.sh
+++ kubernetes/kcompose/1.28.0/ci.sh
@@ -10,5 +10,4 @@ echo "GOPROXY set"
 export GOPROXY=https://goproxy.cn
 
 echo "start build"
-go mod vendor && make bin && \
-       curl -F file=@kompose cloud.loongnix.xa/kubernetes/kcompose/1.28.0
+go mod vendor && make bin && unset http_proxy curl -F file=@kompose cloud.loongnix.xa/kubernetes/kcompose/1.28.0

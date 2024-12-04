341b7ad6795a2daaa1d86237e00048f3273311a1
yzewei <yangzewei@loongson.cn>
2024-12-04 16:45:16 +0800

    add cdi-operator 1.52.0
    Signed-off-by: yzewei <yangzewei@loongson.cn>

---
+++ kubevirt/cdi-operator/1.52.0/ci.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+
+echo "set go env"
+	go env -w GOFLAGS="-buildvcs=false"
+	export GOPROXY=https://goproxy.cn
+
+echo "start build--------------------------------"
+	cd /build/containerized-data-importer-1.52.0 && \
+       	 	go get -u golang.org/x/sys/unix && go mod vendor  &&  \
+		go build -o cdi-operator ./cmd/cdi-operator/operator.go && \
+		bash /build/lib/curl.sh cdi-operator kubevirt/cdi-operator 1.52.0
